709d75d8e006935f597e9ff63aa0977d279d4d7d
Cristiano Belloni <janesconference@bitterspring.net>
2014-02-05 18:01:49 +0000

    Make gain midi-compatible

--- gain-node.js
+++ gain-node.js
@@ -1,4 +1,4 @@
-define(['require'], function() {
+define(['require', 'github.com:mout/mout@0.9.0/src/function/throttle'], function(require, throttle) {
   
     var pluginConf = {
         name: "Gain Node",
@@ -34,12 +34,37 @@ define(['require'], function() {
         this.audioSource.connect(this.gainNode);
         this.gainNode.connect(this.audioDestination);
 
+        this.updateModel = function (param, value) {
+            this.pluginState[param] = value;
+        };
+
+        this.updateGUIView = function (param) {
+            args.hostInterface.setParm (param, this.pluginState[param]);
+        };
+
+        this.updateAudioView = function (param, value, when) {
+            if (param === 'gain') {
+                // Immediate
+                if (!when || !value) {
+                    this.gainNode.gain.value = this.pluginState[param];
+                }
+                else {
+                    // Deferred, use a value
+                    // TODO automate the gain node.
+                }
+            }
+        };
+
+        this.tUpdateGUIView = {
+            "gain": throttle(function () {
+                this.updateGUIView("gain");
+            } , 500).bind(this)
+        };
+
         /* Parameter callbacks */
         var onParmChange = function (id, value) {
-            this.pluginState[id] = value;
-            if (id === 'gain') {
-                this.gainNode.gain.value = value;
-            }
+            this.updateModel(id, value);
+            this.updateAudioView (id);
         };
 
         if (args.initialState && args.initialState.data) {
@@ -55,10 +80,54 @@ define(['require'], function() {
 
         for (var param in this.pluginState) {
             if (this.pluginState.hasOwnProperty(param)) {
-                args.hostInterface.setParm (param, this.pluginState[param]);
-                onParmChange.apply (this, [param, this.pluginState[param]]);
+                this.updateAudioView (param);
+                this.updateGUIView (param);
             }
         }
+
+        var onMIDIMessage = function (message, when) {
+            var now, parmName, setValue, delta;
+            
+            if (message.type === 'controlchange') {
+                /* http://tweakheadz.com/midi-controllers/ */
+                // Using undefined controls
+                if (message.control === 21) {
+                        parmName = "gain";
+                }
+                else {
+                    return;
+                }
+            }
+             
+            if (parmName) {
+
+                setValue = K2.MathUtils.linearRange (message.value / 127 * pluginConf.hostParameters.parameters[parmName].range.max);
+                now = this.context.currentTime;
+                delta = when - now;
+
+                if (delta < 0) {
+                    console.eror ("Gain: Out of time CC Message", delta);
+                }
+                                
+                if (!when || delta < 0) {
+                    // Immediately
+                    this.updateModel(parmName, setValue);
+                    this.updateAudioView (parmName);
+                    this.updateGUIView (parmName);
+                }
+                else {
+                    // Deferred
+                    setTimeout (function() {
+                        // This changes the plugin state and the GUI in the future, loosely 
+                        this.updateModel(parmName, setValue);
+                        this.tUpdateGUIView[parmName]();
+                    }, delta * 1000);
+                    // This changes the audio view in the future
+                    this.updateAudioView (parmName, setValue, when);
+                }
+            }
+        };
+        args.MIDIHandler.setMIDICallback (onMIDIMessage.bind (this));
 
         var saveState = function () {
             return { data: this.pluginState };
